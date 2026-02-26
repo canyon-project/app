94ef825e220de58d9f4cf4dd7713645854f1611a
Travis Zhang <wr.zhang25@gmail.com>
2026-02-26 15:43:33 +0800

    feat: update

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 CREATE SCHEMA IF NOT EXISTS "public";
 
 -- CreateTable
-CREATE TABLE "canyon_next_user" (
+CREATE TABLE "canyon_app_user" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "nickname" TEXT NOT NULL,
@@ -10,11 +10,11 @@ CREATE TABLE "canyon_next_user" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "canyon_next_user_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_repo" (
+CREATE TABLE "canyon_app_repo" (
     "id" TEXT NOT NULL,
     "provider" TEXT NOT NULL,
     "path_with_namespace" TEXT NOT NULL,
@@ -24,27 +24,27 @@ CREATE TABLE "canyon_next_repo" (
     "created_at" TIMESTAMP(3) NOT NULL,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "canyon_next_repo_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_repo_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_commit" (
+CREATE TABLE "canyon_app_commit" (
     "id" TEXT NOT NULL,
     "content" JSONB NOT NULL,
 
-    CONSTRAINT "canyon_next_commit_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_commit_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_merge_request" (
+CREATE TABLE "canyon_app_merge_request" (
     "id" TEXT NOT NULL,
     "content" JSONB NOT NULL,
 
-    CONSTRAINT "canyon_next_merge_request_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_merge_request_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_diff" (
+CREATE TABLE "canyon_app_diff" (
     "id" TEXT NOT NULL,
     "provider" TEXT NOT NULL,
     "repo_id" TEXT NOT NULL,
@@ -56,11 +56,11 @@ CREATE TABLE "canyon_next_diff" (
     "subject" TEXT NOT NULL,
     "subject_id" TEXT NOT NULL,
 
-    CONSTRAINT "canyon_next_diff_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_diff_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_infra_config" (
+CREATE TABLE "canyon_app_infra_config" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "value" TEXT NOT NULL,
@@ -69,11 +69,11 @@ CREATE TABLE "canyon_next_infra_config" (
     "isEncrypted" BOOLEAN NOT NULL DEFAULT false,
     "lastSyncedEnvFileValue" TEXT,
 
-    CONSTRAINT "canyon_next_infra_config_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_infra_config_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_coverage" (
+CREATE TABLE "canyon_app_coverage" (
     "id" TEXT NOT NULL,
     "builds" JSONB NOT NULL,
     "build_hash" TEXT NOT NULL,
@@ -87,11 +87,11 @@ CREATE TABLE "canyon_next_coverage" (
     "created_at" TIMESTAMP(3) NOT NULL,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "canyon_next_coverage_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_coverage_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_coverage_map_relation" (
+CREATE TABLE "canyon_app_coverage_map_relation" (
     "id" TEXT NOT NULL,
     "full_file_path" TEXT NOT NULL,
     "restore_full_file_path" TEXT NOT NULL,
@@ -100,28 +100,28 @@ CREATE TABLE "canyon_next_coverage_map_relation" (
     "source_map_hash" TEXT NOT NULL,
     "file_content_hash" TEXT NOT NULL,
 
-    CONSTRAINT "canyon_next_coverage_map_relation_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_coverage_map_relation_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_coverage_map" (
+CREATE TABLE "canyon_app_coverage_map" (
     "hash" TEXT NOT NULL,
     "map" BYTEA NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "canyon_next_coverage_map_pkey" PRIMARY KEY ("hash")
+    CONSTRAINT "canyon_app_coverage_map_pkey" PRIMARY KEY ("hash")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_coverage_source_map" (
+CREATE TABLE "canyon_app_coverage_source_map" (
     "hash" TEXT NOT NULL,
     "source_map" BYTEA NOT NULL,
 
-    CONSTRAINT "canyon_next_coverage_source_map_pkey" PRIMARY KEY ("hash")
+    CONSTRAINT "canyon_app_coverage_source_map_pkey" PRIMARY KEY ("hash")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_coverage_hit" (
+CREATE TABLE "canyon_app_coverage_hit" (
     "id" TEXT NOT NULL,
     "build_hash" TEXT NOT NULL,
     "scene_key" TEXT NOT NULL,
@@ -132,15 +132,15 @@ CREATE TABLE "canyon_next_coverage_hit" (
     "input_source_map" INTEGER,
     "created_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "canyon_next_coverage_hit_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "canyon_app_coverage_hit_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "canyon_next_coverage_lock" (
+CREATE TABLE "canyon_app_coverage_lock" (
     "coverage_id" TEXT NOT NULL,
     "locked_at" TIMESTAMP(3) NOT NULL,
     "locked_by" TEXT NOT NULL,
 
-    CONSTRAINT "canyon_next_coverage_lock_pkey" PRIMARY KEY ("coverage_id")
+    CONSTRAINT "canyon_app_coverage_lock_pkey" PRIMARY KEY ("coverage_id")
 );
 
