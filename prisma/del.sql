1ca65a0f1217d17a8e1c4bc0a5bbff4bdd26c915
Travis Zhang <wr.zhang25@gmail.com>
2026-03-02 17:33:42 +0800

    chore: daily development

--- prisma/del.sql
+++ prisma/del.sql
@@ -1,5 +1,5 @@
-delete from canyon_app_coverage;
-delete from canyon_app_coverage_hit;
-delete from canyon_app_coverage_map;
-delete from canyon_app_coverage_map_relation;
-delete from canyon_app_coverage_source_map;
+delete from canyon_next_coverage;
+delete from canyon_next_coverage_hit;
+delete from canyon_next_coverage_map;
+delete from canyon_next_coverage_map_relation;
+delete from canyon_next_coverage_source_map;
